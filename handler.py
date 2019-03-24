7eed6c0b42d7d77ad0a0c5d803a1460cca526197
Sergey_26rus <Sergey.26rus.ess@gmail.com>
2019-03-24 19:13:20 +0300

    Added comments. Some corrections in the code and in the grammar ( fuck my english:) )

--- handler.py
+++ handler.py
@@ -1,21 +1,19 @@
 import os
 import re
 import time
-import json
 import webbrowser
-from typing import Tuple
 import steam.webauth as wa
 from bs4 import BeautifulSoup as bs
 
 
 def log(text: str, printdate: bool = True, printmessage: bool = True, writetofile: bool = True) -> str:
-    """
+    """Prints the text in the console and saves it in log file
 
-    :param text: The text itself
-    :param printdate: Do print date in message?
-    :param printmessage: Do print message in console or not?
-    :param writetofile: Do write message in file or not?
-    :return: The modified text
+    :param text: text you want to print
+    :param printdate: do print date in message?
+    :param printmessage: do print message in console or not?
+    :param writetofile: do write message in file or not?
+    :return: a modified text. For example for input()
     """
 
     if printdate:
@@ -28,7 +26,7 @@ def log(text: str, printdate: bool = True, printmessage: bool = True, writetofil
         print(message)
 
     if writetofile:
-        f = open(os.path.dirname(__file__) + "py-antigay.log", "a")
+        f = open(os.path.dirname(__file__) + "/" + "py-antigay.log", "a")
         f.write(message + "\n")
         f.close()
 
@@ -36,7 +34,9 @@ def log(text: str, printdate: bool = True, printmessage: bool = True, writetofil
 
 
 class Handler:
-    def __init__(self, login, password, content_id, author_id, words_blacklist, users_blacklist=(), users_whitelist=()):
+    def __init__(self, login: str, password: str, content_id: str, author_id: str, words_blacklist: tuple,
+                 users_blacklist: tuple = (), users_whitelist: tuple = ()) -> None:
+
         self.__user = wa.WebAuth(login, password)
         self.__session = None
 
@@ -49,6 +49,7 @@ class Handler:
         self.set_session()
 
     def set_session(self):
+        """Authorizing in Steam and saving session"""
         log("Setting up the session for work")
 
         captcha = ""
@@ -62,11 +63,11 @@ class Handler:
                 log("Opening captcha image.. {captcha_url}".format(captcha_url=self.__user.captcha_url),
                     writetofile=False)
                 webbrowser.open(self.__user.captcha_url)
-                captcha = input(log("Enter the captcha> ", False, False, False))
+                captcha = input(log("Enter the captcha>\n", False, False, False))
             except wa.EmailCodeRequired:
-                email_code = input(log("Enter Steam Guard E-mail code> ", False, False, False))
+                email_code = input(log("Enter Steam Guard E-mail code>\n", False, False, False))
             except wa.TwoFactorCodeRequired:
-                twofactor_code = input(log("Enter Mobile Steam Guard code> ", False, False, False))
+                twofactor_code = input(log("Enter Mobile Steam Guard code>\n", False, False, False))
             except wa.LoginIncorrect:
                 log("Incorrect user name or password. Or login limit exceeded")
             except KeyboardInterrupt:
@@ -77,19 +78,26 @@ class Handler:
         self.__session = self.__user.login()
 
     def get_session(self):
+        """Just returning the session"""
         return self.__session
 
     def delete_comment(self, gid_comment):
+        """Sends POST request to the Steam servers for deletion
+
+        :param gid_comment: a GID of comment
+        """
         session = self.get_session()
+
+        # session.cookies.get() will raise you exception CookieConflictError, so I must use dict with cookies directly
         cookies = session.cookies.get_dict()
 
-        headers = {
+        """headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36",
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
             "Accept-Encoding": "gzip, deflate, br",
             "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
             "DNT": "1",
-        }
+        }"""
         params = {
             "count": 10,
             "extended_data": "{'contributors':['" + self.author_id +"'],'appid':4000,'sharedfile':{'m_parentsDetails':[],'m_parentBundlesDetails':[],'m_bundledChildren':[],'m_ownedBundledItems':[]}}",
@@ -104,7 +112,7 @@ class Handler:
                 author_id=self.author_id,
                 content_id=self.content_id
             ),
-            headers=headers, data=params).json()
+            data=params).json()
 
         if post.get("success", False):
             log("Comment deletion successful (GID: {})".format(gid_comment))
@@ -112,6 +120,7 @@ class Handler:
             log("Comment deletion failed (GID: {})".format(gid_comment))
 
     def parse_comments(self):
+        """Getting workshop page and checking comments for bad things"""
         session = self.get_session()
 
         # Getting workshop page
@@ -142,7 +151,7 @@ class Handler:
 
             # Looking for annoying users in blacklist and delete comment if found something
             if mini_profile_id in self.USERS_BLACKLIST:
-                log("A fagot has found (Name: {name}, Steam Link: {link}, Mini Profile Id: {mini})".format(
+                log("A fagot found (Name: {name}, Steam Link: {link}, Mini Profile Id: {mini})".format(
                     name=profile_name,
                     link=profile_link,
                     mini=mini_profile_id))
@@ -152,7 +161,7 @@ class Handler:
             # And now looking for blacklist words in comment and delete it if found something
             for word in self.WORDS_BLACKLIST:
                 if text.lower().find(word) != -1:
-                    log("A fagot has found (Name: {name}, Steam Link: {link}, Mini Profile Id: {mini})".format(
+                    log("A fagot found (Name: {name}, Steam Link: {link}, Mini Profile Id: {mini})".format(
                         name=profile_name,
                         link=profile_link,
                         mini=mini_profile_id))
